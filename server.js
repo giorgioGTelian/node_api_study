66ee8c1c526a781319a11a8ebcff83b3c58505c9
giorgio <giorgio.programmer@gmail.com>
2025-01-03 10:59:19 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bookRoutes = require("./routes/books");
+
+const app = express();
+app.use(bodyParser.json());
+app.use("/books", bookRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
