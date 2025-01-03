477ad43bac4ba22cec32711d12dd1fb2f43d42c2
giorgio <giorgio.programmer@gmail.com>
2025-01-03 10:58:22 +0100

    Create books.js

---
+++ data/books.js
@@ -0,0 +1,6 @@
+const books = [
+  { isbn: "12345", title: "Book One", author: "Author One", reviews: [] },
+  { isbn: "67890", title: "Book Two", author: "Author Two", reviews: [] },
+];
+
+module.exports = books;
